5a34356d6e6a01474831986dd23406b8a5d23674
fmynj <yle151455@gmail.com>
2025-11-04 13:39:47 +0800

    更新 Admin.vue

--- web/src/views/Admin.vue
+++ web/src/views/Admin.vue
@@ -84,7 +84,7 @@
         <UserManage v-if="page==='user'" />
       </div>
       <footer class="admin-footer">
-        <p class="admin-copyright">Copyright © 2025 Nav-Item | <a href="https://github.com/eooce/Nav-Item" target="_blank" class="footer-link">Powered by eooce</a></p>
+        <p class="admin-copyright">Copyright © 2025 Nav-Item | <a href="https://github.com/fmynj/Nav-Item" target="_blank" class="footer-link">Powered by fmynj</a></p>
       </footer>
     </main>
   </div>
